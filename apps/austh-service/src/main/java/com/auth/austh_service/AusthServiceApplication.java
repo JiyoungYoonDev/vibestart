87974bb587b11e25bc6c2349492a25a749713533
ebbuni1023 <97073638+CodingJiyoung@users.noreply.github.com>
2026-07-02 17:03:22 -0500

    feat: add initial structure for Austh Service with Maven and Spring Boot

---
+++ apps/austh-service/src/main/java/com/auth/austh_service/AusthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.auth.austh_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AusthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AusthServiceApplication.class, args);
+	}
+
+}
---
+++ apps/austh-service/src/test/java/com/auth/austh_service/AusthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth.austh_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AusthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
