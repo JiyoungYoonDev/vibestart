42524d34c516810f9c82521c5a6cab8e94e27a0f
ebbuni1023 <97073638+CodingJiyoung@users.noreply.github.com>
2026-07-02 16:54:43 -0500

    initial commit

---
+++ app/vibestarter-api/src/main/java/com/vibestart/api/VibestarterApiApplication.java
@@ -0,0 +1,13 @@
+package com.vibestart.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibestarterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibestarterApiApplication.class, args);
+	}
+
+}
